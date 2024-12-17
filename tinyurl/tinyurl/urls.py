ccf35625b0631869d5a348d03d1b3ab85729ba50
anuragmahto <anuragmahto85@outlook.com>
2024-12-17 23:56:44 +0530

    WIP adding django code to host urlshorter app

--- tinyurl/tinyurl/urls.py
+++ tinyurl/tinyurl/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("urlapp/", include("urlapp.urls")),
+    path('', include("urlapp.urls")),
     path('admin/', admin.site.urls),
 ]
--- tinyurl/urlapp/views.py
+++ tinyurl/urlapp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
-def index(request):
-    return HttpResponse("Welcome page of Tiny URL!!")
+def index(self):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
